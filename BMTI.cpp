6c07714d56f1243bebcdb3ab14b3de31adb802eb
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-03-10 18:28:51 +0800

    Menjalankan Int main

--- BMTI.cpp
+++ BMTI.cpp
@@ -26,3 +26,23 @@ string statusBMI(float bmi) {
         return "Obesitas";
     }
 }
+
+
+int main() {
+    float berat, tinggi, bmi;
+
+    cout << "--- Selamat Datang Mahasiswa Kelas B 2025 ---" << endl;
+
+    // Memanggil prosedur input
+    inputData(berat, tinggi);
+
+    // Menghitung BMI
+    bmi = hitungBMI(berat, tinggi);
+
+    cout << endl;
+    cout << "--- Hasil ---" << endl;
+    cout << "BMI Anda : " << bmi << endl;
+    cout << "Status   : " << statusBMI(bmi) << endl;
+
+    return 0;
+}
